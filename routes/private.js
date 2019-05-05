18f9624ee43aadb846effca236d285bea76e51e1
Chloé Leteinturier <chloe.leteinturier@gmail.com>
2019-05-05 10:06:23 +0200

    favorites list dynamic

--- routes/private.js
+++ routes/private.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+router.use((req, res, next) => {
+  console.log('req.path', req.path);
+
+  if (req.session.currentUser) { // <== if there's user in the session (user is logged in)
+    next(); // ==> go to the next route ---
+  } else {
+    res.redirect('/login');
+  }
+});
+
 /* GET edit profile form . */
 router.get('/edit-profile', (req, res, next) => res.render('private/edit-profile'));
 
 /* GET list of favorites. */
-router.get('/favorites', (req, res, next) => res.render('private/favorites'));
+router.get('/favorites', (req, res, next) => {
+  console.log('req.session.currentUser');
 
-
+  res.render('private/favorites', { name: req.session.currentUser.name });
+});
 
 module.exports = router;
