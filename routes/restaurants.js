2bcba9aed43912260186d30f8016759e63887381
Fabio Núñez <fabionunez@gmail.com>
2019-05-06 14:16:51 +0200

    refactoring the results

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,62 +2,47 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios'); // to use the API
 
-const feedPhoto = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=`;
+// const feedPhoto = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=`;
 
 /* GET restaurants listing. */
 router.get('/', (req, res, next) => {
-  const { type, price } = req.query;
+  const { type, price, pagetoken } = req.query;
 
-  axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?maxprice=${price}&minprice=${price}&keyword=${type}&location=41.387098,%202.165746&radius=5000&type=restaurant&key=${process.env.API_KEY}`)
+  let pagetokenParam = '';
+  if (pagetoken !== undefined) {
+    pagetokenParam = 'pagetoken=' + pagetoken + '&';
+  }
+
+  axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?${pagetokenParam}maxprice=${price}&minprice=${price}&keyword=${type}&location=41.387098,%202.165746&radius=5000&type=restaurant&key=AIzaSyCjoxAmGGvyGMVLx8jHkzSQTdfz8F1rknw`)
     .then(function (response) {
-      // console.log(response.data.results[0].photos[0].photo_reference);
-      // const restaurants = response.data.results;
+      const newRestaurants = response.data;
+
       // for (var i = 0; i < restaurants.length; i++) {
-      //   console.log(restaurants[i].photos[0].photo_reference);
+      //   // console.log(restaurants[i].photos[0].photo_reference);
+      //   newRestaurants.push({
+      //     name: response.data.results[i].name,
+      //     place_id: response.data.results[i].place_id,
+      //     price_level: response.data.results[i].price_level,
+      //     rating: response.data.results[i].rating,
+      //     user_ratings_total: response.data.results[i].user_ratings_total,
+      //     next_page_token: response.data.next_page_token,
+      //     type: type,
+      //     price: price,
+      //     main_photo: `${feedPhoto}${response.data.results[i].photos[0].photo_reference}&key=${process.env.API_KEY}`
+      //   });
       // }
 
-      const restaurants = response.data.results;
-      const newRestaurants = [];
+      newRestaurants.type = type;
+      newRestaurants.price = price;
+      // newRestaurants.numPage = numPage;
 
-      for (var i = 0; i < restaurants.length; i++) {
+      for (var i = 0; i < newRestaurants.results.length; i++) {
         // console.log(restaurants[i].photos[0].photo_reference);
-        newRestaurants.push({
-          name: response.data.results[i].name,
-          place_id: response.data.results[i].place_id,
-          price_level: response.data.results[i].price_level,
-          rating: response.data.results[i].rating,
-          user_ratings_total: response.data.results[i].user_ratings_total,
-          next_page_token: response.data.next_page_token,
-          type: type,
-          price: price,
-          main_photo: `${feedPhoto}${response.data.results[i].photos[0].photo_reference}&key=${process.env.API_KEY}`
-        });
+        console.log(newRestaurants.results[i].photos[0].photo_reference);
       }
 
-      // for (var i = 0; i < newRestaurants.length; i++) {
-      //   // console.log(newRestaurants[i].photosArr);
-      //   for (var j = 0; j < newRestaurants[i].photosArr.length; j++) {
-      //     // console.log(newRestaurants[i].photosArr[j].photo_reference);
-      //     newRestaurants.push({
-      //       photo_reference: newRestaurants[i].photosArr[j].photo_reference
-      //     });
-      //   }
-      // }
-
-      // let imageURLPromises = response.data.results.map((result) => {
-      //   // console.log(`${feedPhoto}${result.photos[0].photo_reference}&key=${process.env.API_KEY}`);
-      //   axios.get(`${feedPhoto}${result.photos[0].photo_reference}&key=${process.env.API_KEY}`);
-      //   // return axios.get(`${feedPhoto}${result.photos[0].photo_reference}&key=${process.env.API_KEY}`);
-      // });
-
-      console.log(newRestaurants);
-      // return newRestaurants;
       res.render('restaurants/restaurant-list', { newRestaurants });
     })
-    // .then((newRestaurants) => Promise.all(newRestaurants))
-    // .then((newRestaurants) => {
-    //   res.render('restaurants/restaurant-list', { newRestaurants });
-    // })
     .catch(function (error) {
       console.log(error);
     });
@@ -67,7 +52,7 @@ router.get('/', (req, res, next) => {
 router.get('/details', (req, res, next) => {
   const { place_id } = req.query;
   // console.log(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${process.env.API_KEY}`);
-  axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${process.env.API_KEY}`)
+  axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=AIzaSyCjoxAmGGvyGMVLx8jHkzSQTdfz8F1rknw`)
     .then(function (response) {
       const restaurantDetails = response.data.result;
       // console.log(restaurantDetails);
